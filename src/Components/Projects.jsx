35bb787cd53f6cfffa88d6c31dd4fffafa506edb
mariem <mariamkhaled709@gmail.com>
2024-06-28 08:44:23 +0300

    style-issue

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -8,8 +8,8 @@ const Projects = () => {
         <h3 title='projects'>
             Some of my recent projects
         </h3>
-        <div class="row1-container">
-      <div class="card ">
+        <div className="row1-container">
+      <div className="card ">
       <h4>Blop CRUD App</h4><img src="https://assets.codepen.io/2301174/icon-supervisor.svg" alt=""/>
       <p>a performance-efficent app ustilisng State shape normalisation by Redux to add,delete and edit your posts</p>
       <div className="flex">
@@ -19,7 +19,7 @@ const Projects = () => {
       
     </div>
 
-    <div class="card">
+    <div className="card">
       <h4>Tenzies Game</h4>
       <img src="https://assets.codepen.io/2301174/icon-team-builder.svg" alt=""/>
       <p>Roll the dice to see if you are going to win</p>
@@ -30,7 +30,7 @@ const Projects = () => {
 
     </div>
 
-    <div class="card ">
+    <div className="card ">
     <h4>Notes App</h4>
     <img src="https://assets.codepen.io/2301174/icon-team-builder.svg" alt=""/>
       <p>Note editor app to save your notes and style them however you like</p>
@@ -41,8 +41,8 @@ const Projects = () => {
       
     </div>
   </div>
-  <div class="row2-container">
-    <div class="card ">
+  <div className="row2-container">
+    <div className="card ">
       <h4>Vans App</h4>
       <img src="https://assets.codepen.io/2301174/icon-karma.svg" alt="vans-website"/>
       <p>A renting app  implemented with React Router for smooth navigation and optimisied user exerperince </p>
