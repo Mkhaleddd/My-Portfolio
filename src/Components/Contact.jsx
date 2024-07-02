1393a4b92113fe517a88a86151225d7e9896fa26
mariem <mariamkhaled709@gmail.com>
2024-07-02 14:52:37 +0300

    fixy-fix

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,6 @@
 
 import { useRef } from 'react';
-import {emailjs} from '@emailjs/browser';
+import emailjs from '@emailjs/browser';
 import {IoIosSend} from "react-icons/io";
 
 const Contact = () => {
