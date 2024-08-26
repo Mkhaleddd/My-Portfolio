7804f4d68d8f8372b9398acd0cfce6b462e40707
mariem <mariamkhaled709@gmail.com>
2024-08-26 13:33:51 +0300

    fix stylinf issues in scrolling and design

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,15 +7,10 @@ const About = () => {
         <h3 title='about'>About me</h3>
         <div className="wrapper">
            <span className="type">
-                A Frontend Developer enthusiast with a strong desire to create engaging and 
-                user-friendly digital experiences with a solid foundation in HTML, CSS, and 
-                 JavaScript, I thrive on transforming ideas into intuitive interfaces that resonate 
-                with users. Throughout my journey, I've honed my skills in responsive design, UI/
-                UX principles, and front-end frameworks like React . I'm eager to contribute my 
-                creativity and technical skills to projects that push boundaries and deliver 
-                exceptional user experiences and able to reduce time-to-value on critical new
-                skill-sets by rapidly mastering and adopting groundbreaking technology,business concepts and industry knowledge.
-    </span>
+            Hi, I'm Mariam, a Frontend Developer with a passion
+            for creating polished user interfaces
+             and delightful digital high-performace experiences. 
+            </span>
         
         </div>
        
